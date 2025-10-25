6bac1302908b244f68ec2a24b77f133c097190d0
STRAMIRA <mira.strachota@email.cz>
2025-10-25 09:57:55 +0200

    testovaci prostredi

---
+++ test.py
@@ -0,0 +1,18 @@
+print("Tohle je test")
+print(type("String?"))
+
+print("@")
+print(type("@"))
+
+print(type(5))
+
+print(type(5.001))
+
+print(type(input("cozeeee")))
+
+knihovna = {"jmeno": "Mira", "registrovany": True, "vek": 33}
+print(knihovna)
+
+cislo = 5
+for cislo in range(20):
+    print(cislo)
